286ddb8aa6e71f116099d9002b46a5345c986c7e
JUAN CARLOS CATTANEO <juancattaneo92@gmail.com>
2020-05-18 02:46:22 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,13 @@
-# Write your code here.
+# Write your code here
+
+def take_a_number(katz_deli, name)
+  puts "Welcome #{name}. You are number #{katz_deli} in line."
+end
+
+def line(katz_deli="empty")
+  puts "The line is currently " + "katz_deli" + "."
+end
+
+def now_serving(katz_deli)
+  #"Currently serving NAME".
+end
